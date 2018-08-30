067152dbe7d26f9629fcbef5ce8d30be64d317d0
andresSaldanaAguilar <pollyketpo@gmail.com>
2018-08-30 18:54:31 -0500

    :sparkles: figuras con movimiento en 3D

--- MiCubo/src/MidletCono.java
+++ MiCubo/src/MidletCono.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.AlertType;
@@ -15,18 +10,18 @@ import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.midlet.*;
 
-public class CuboMidlet extends MIDlet implements CommandListener{
+public class MidletCono extends MIDlet implements CommandListener{
 
     private Display d;
     Canvas ca;
     private Command cs;
 
-    public CuboMidlet(){
+    public MidletCono(){
         d = Display.getDisplay(this); 
 
         ca = new Canvas(){
             int centerX, centerY, maxX, maxY, minMaxXY;
-            Obj obj = new Obj();
+            ObjCono obj = new ObjCono();
             int x=0, y=0;
             public void paint(Graphics g){             
                 g.setColor(0x5B98FF);
@@ -36,15 +31,22 @@ public class CuboMidlet extends MIDlet implements CommandListener{
                 centerY = maxY/2;
                 obj.d = obj.rho*minMaxXY/obj.objSize;
                 obj.eyeAndScreen();
-                line(g, 0, 1); line(g, 1, 2); line(g, 2, 3); line(g, 3, 0); // aristas horizontales inferiores
-                line(g, 4, 5); line(g, 5, 6); line(g, 6, 7); line(g, 7, 4); // aristas horizontales superiores
-                line(g, 0, 4); line(g, 1, 5); line(g, 2, 6); line(g, 3, 7); // aristas verticales
+                line(g, 0, 1); line(g, 1, 2); line(g, 2, 3); line(g, 3, 4); line(g, 4, 5); line(g, 5, 6); //base
+                line(g, 6, 7); line(g, 7, 0); //base
+                line(g, 0, 8); line(g, 1, 8); line(g, 2, 8); line(g, 3, 8); line(g, 4, 8); line(g, 5, 8);
+                line(g, 6, 8); line(g, 7, 8); //punta                
             }
             void line(Graphics g, int i, int j){
                 Point2D p = obj.vScr[i], q = obj.vScr[j];
                 System.out.println(((int)p.x+centerX));
                 g.setColor(255,255,255);
                 g.drawLine(centerX+(int)p.x ,centerY-(int)p.y, centerX+(int)q.x, centerY-(int)q.y);
+            }
+            void arc(Graphics g, int i, int j){
+                Point2D p = obj.vScr[i], q = obj.vScr[j];
+                System.out.println(((int)p.x+centerX));
+                g.setColor(255,255,255);
+                g.drawArc(centerX+(int)p.x ,centerY-(int)p.y, 100, 100,180,90);
             }
             public void keyPressed(int keyCode){
             switch (getGameAction(keyCode)) {
@@ -98,24 +100,25 @@ public class CuboMidlet extends MIDlet implements CommandListener{
     } 
 }
 
-class Obj{	// Posee los datos del objeto 3D
+class ObjCono{	// Posee los datos del objeto 3D
     float rho, theta=0.3F, phi=1.3F, d, objSize, v11, v12, v13, v21, v22, v23, v32, v33, v43; // elementos de la matriz V
     Point3D [] w;	// coordenadas universales
     Point2D [] vScr; // coordenadas de la pantalla
-    Obj(){	// CAMBIAR LAS COORDENADAS X,Y,Z CON 0,1 PARA CONSTRUIR PRISMA, CILINDRO, PIRAMIDE, CONO Y ESFERA.
-        w	= new Point3D[8];
-	vScr	= new Point2D[8];
+    ObjCono(){	// CAMBIAR LAS COORDENADAS X,Y,Z CON 0,1 PARA CONSTRUIR PRISMA, CILINDRO, PIRAMIDE, CONO Y ESFERA.
+        w	= new Point3D[9];
+	vScr	= new Point2D[9];
         
-        //Cubo
-        w[0]	= new Point3D(1, -1, -1); // desde la base
-	w[1]	= new Point3D(1, 1, -1);
-	w[2]	= new Point3D(-1, 1, -1);
-	w[3]	= new Point3D(-1, -1, -1);
-	w[4]	= new Point3D(1, -1, 1);
-	w[5]	= new Point3D(1, 1, 1);
-	w[6]	= new Point3D(-1, 1, 1);
-	w[7]	= new Point3D(-1, -1, 1);
-        
+        //base
+        w[0]	= new Point3D(1, -1, -1); //0
+        w[1]	= new Point3D(1.5, 0, -1);//2
+	w[2]	= new Point3D(1, 1, -1); //1
+        w[3]	= new Point3D(0, 1.5, -1);//3
+	w[4]	= new Point3D(-1, 1, -1);//4
+        w[5]	= new Point3D(-1.5, 0, -1);//5
+	w[6]	= new Point3D(-1, -1, -1);//6
+	w[7]	= new Point3D(0, -1.5, -1);//7
+        w[8]	= new Point3D(0, 0, 2);//8
+    
 	objSize = (float) Math.sqrt(12F); // = sqrt(2*2 + 2*2 + 2*2) es la distancia entre dos vertices opuestos
 	rho	= 5*objSize;		// para cambiar la perspectiva
     }
@@ -127,7 +130,7 @@ class Obj{	// Posee los datos del objeto 3D
     }
     void eyeAndScreen(){
         initPersp();
-	for(int i=0; i<8; i++){
+	for(int i=0; i<9; i++){
             Point3D p = w[i];
             float x = v11*p.x + v21*p.y, y = v12*p.x + v22*p.y + v32*p.z, z = v13*p.x + v23*p.y + v33*p.z + v43;
             vScr[i] = new Point2D(-d*x/z, -d*y/z);
@@ -135,19 +138,3 @@ class Obj{	// Posee los datos del objeto 3D
     }
 }
 
-class Point2D{
-    float x, y;
-    Point2D(float x, float y){
-    	this.x = x;
-        this.y = y;
-    }
-}
-
-class Point3D{
-    float x, y, z;
-    Point3D(double x, double y, double z){
-        this.x = (float) x;
-	this.y = (float) y;
-	this.z = (float) z;
-    }
-}
--- MiCubo/src/MidletPiramide.java
+++ MiCubo/src/MidletPiramide.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 
+import javax.microedition.midlet.*;
+
+
 import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Canvas;
@@ -15,18 +18,18 @@ import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.midlet.*;
 
-public class CuboMidlet extends MIDlet implements CommandListener{
+public class MidletPiramide extends MIDlet implements CommandListener{
 
     private Display d;
     Canvas ca;
     private Command cs;
 
-    public CuboMidlet(){
+    public MidletPiramide(){
         d = Display.getDisplay(this); 
 
         ca = new Canvas(){
             int centerX, centerY, maxX, maxY, minMaxXY;
-            Obj obj = new Obj();
+            ObjPiramide obj = new ObjPiramide();
             int x=0, y=0;
             public void paint(Graphics g){             
                 g.setColor(0x5B98FF);
@@ -98,23 +101,23 @@ public class CuboMidlet extends MIDlet implements CommandListener{
     } 
 }
 
-class Obj{	// Posee los datos del objeto 3D
+class ObjPiramide{	// Posee los datos del objeto 3D
     float rho, theta=0.3F, phi=1.3F, d, objSize, v11, v12, v13, v21, v22, v23, v32, v33, v43; // elementos de la matriz V
     Point3D [] w;	// coordenadas universales
     Point2D [] vScr; // coordenadas de la pantalla
-    Obj(){	// CAMBIAR LAS COORDENADAS X,Y,Z CON 0,1 PARA CONSTRUIR PRISMA, CILINDRO, PIRAMIDE, CONO Y ESFERA.
+    ObjPiramide(){	// CAMBIAR LAS COORDENADAS X,Y,Z CON 0,1 PARA CONSTRUIR PRISMA, CILINDRO, PIRAMIDE, CONO Y ESFERA.
         w	= new Point3D[8];
 	vScr	= new Point2D[8];
         
-        //Cubo
+        //Base
         w[0]	= new Point3D(1, -1, -1); // desde la base
 	w[1]	= new Point3D(1, 1, -1);
 	w[2]	= new Point3D(-1, 1, -1);
 	w[3]	= new Point3D(-1, -1, -1);
-	w[4]	= new Point3D(1, -1, 1);
-	w[5]	= new Point3D(1, 1, 1);
-	w[6]	= new Point3D(-1, 1, 1);
-	w[7]	= new Point3D(-1, -1, 1);
+	w[4]	= new Point3D(0, 0, 1);
+	w[5]	= new Point3D(0, 0, 1);
+	w[6]	= new Point3D(0, 0, 1);
+	w[7]	= new Point3D(0, 0, 1);
         
 	objSize = (float) Math.sqrt(12F); // = sqrt(2*2 + 2*2 + 2*2) es la distancia entre dos vertices opuestos
 	rho	= 5*objSize;		// para cambiar la perspectiva
@@ -135,19 +138,4 @@ class Obj{	// Posee los datos del objeto 3D
     }
 }
 
-class Point2D{
-    float x, y;
-    Point2D(float x, float y){
-    	this.x = x;
-        this.y = y;
-    }
-}
 
-class Point3D{
-    float x, y, z;
-    Point3D(double x, double y, double z){
-        this.x = (float) x;
-	this.y = (float) y;
-	this.z = (float) z;
-    }
-}
