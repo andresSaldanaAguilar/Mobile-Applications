3f88cd2614bd555a2748f1f6dd07ce90f25b5e18
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2018-09-06 18:22:50 -0500

    :sparkles: tipos de layouts

---
+++ Android Projects/Layouts/app/src/main/java/com/example/andressaldana/factorial/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.andressaldana.factorial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        configureNextButton();
+    }
+
+    public void configureNextButton(){
+        Button nextButton = (Button) findViewById(R.id.BtnAdelante2);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this, ThirdActivity.class));
+            }
+        });
+    }
+}
