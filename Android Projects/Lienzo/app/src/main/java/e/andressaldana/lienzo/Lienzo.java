72a614325ab27c935525199b012484eef2f57188
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2018-10-17 17:47:39 -0500

    triangulo de sierpinski

--- Android Projects/Lienzo/app/src/main/java/e/andressaldana/lienzo/Lienzo.java
+++ Android Projects/Lienzo/app/src/main/java/e/andressaldana/lienzo/Lienzo.java
@@ -3,6 +3,10 @@ package e.andressaldana.lienzo;
 import android.content.*;
 import android.graphics.*;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Lienzo extends View{
     Paint p;
     int x, y;
@@ -18,19 +22,56 @@ public class Lienzo extends View{
         c.drawPaint(p);
         p.setColor(Color.BLACK); // Texto negro
         p.setTextSize(40);
-        c.drawText("x0=" + x/2 + ", y0=" + y/2, x/2 + 20, y/2-20, p);
         p.setColor(Color.rgb(0, 0, 255)); // Ejes azules
-        c.drawLine(x/2, 0, x/2, y, p);
-        c.drawLine(0, y/2, x, y/2, p);
+        //c.drawLine(x/2, 0, x/2, y, p);
+        //c.drawLine(0, y/2, x, y/2, p);
+        c.drawText(x+","+y,10,40,p);
 
-        p.setTextAlign(Paint.Align.valueOf("RIGHT"));
-        p.setTypeface(Typeface.MONOSPACE);
-        c.drawText("Eje X", x, y/2, p);
-        p.setTextAlign(Paint.Align.valueOf("RIGHT"));
-        p.setTypeface(Typeface.MONOSPACE);
-        c.drawText("Eje Y", x/2, y, p);
+        ArrayList<ArrayList<Float>> vector2D = new ArrayList<ArrayList<Float>>();
+        ArrayList<Float> v1 = new ArrayList<Float>();
+        v1.add((float) (1*x/4));
+        v1.add((float) (5*y/8));
+        ArrayList<Float> v2 = new ArrayList<Float>();
+        v2.add((float) (3*x/4));
+        v2.add((float) (5*y/8));
+        ArrayList<Float> v3 = new ArrayList<Float>();
+        v3.add((float) (x/2));
+        v3.add((float) ((5*y/8)-467.65));
+        ArrayList<Float> out = new ArrayList<Float>();
+        out.add((float) (1*x/4));
+        out.add((float) ((5*y/8)-467.65));
+        vector2D.add(v1);
+        vector2D.add(v2);
+        vector2D.add(v3);
+        vector2D.add(out);
 
-        p.setColor(Color.argb(100, 200, 100, 100));
-        c.drawCircle(x/2-120, y/2+120, 100, p);
+        c.drawCircle(1*x/4,5*y/8,5,p);
+        c.drawCircle(3*x/4,5*y/8,5,p);
+        c.drawCircle(x/2, (float) ((5*y/8)-467.65),5,p);
+        c.drawCircle(1*x/4,(float) ((5*y/8)-467.65),5,p);
+        Random rnd = new Random();
+        for(int i = 0; i < 100000; i++){
+            ArrayList<Float> aux = new ArrayList<Float>();
+            p.setColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+            aux = getMP(vector2D.get(getRandBetween()),vector2D.get(3));
+            System.out.println(getRandBetween());
+            c.drawCircle(aux.get(0),aux.get(1),5,p);
+            vector2D.set(3,aux);
+        }
+
+    }
+
+    public ArrayList<Float> getMP(ArrayList<Float> p1, ArrayList<Float> p2){
+        ArrayList<Float> aux = new ArrayList<Float>();
+        aux.add((p1.get(0)+p2.get(0))/2);
+        aux.add((p1.get(1)+p2.get(1))/2);
+        return aux;
+    }
+
+    public int getRandBetween(){
+        Random r = new Random();
+        int Low = 0;
+        int High = 3;
+        return r.nextInt(High-Low) + Low;
     }
 }
